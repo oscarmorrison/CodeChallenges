295bf0c6afc95d4b28f432eaad4ad382391acb93
Oscar Morrison <me@oscarmorrison.com>
2015-09-18 22:46:47 -0700

    Letter Histogram, Word, and Bigram

--- 25LetterHistogram.rb
+++ 25LetterHistogram.rb
@@ -2,20 +2,29 @@
 
 file = File.open("25in.txt","r").readlines
 
-##Count all the amount of letters in file
+###Count all the amount of letters in file
 #letter_count = Hash.new(0)
 #file.each do |line|
 #	line.gsub(/\s|[^a-zA-Z]/,'').downcase.each_char do |char|
-#		letter_count[char] = letter_count[char] + 1
+#		letter_count[char] += 1
 #	end
 #end
 #letter_count.sort_by{|k,v|-v}[0..10].each{|k,v| puts "#{k}: #{v}"}
+#
+###Count occurences of single word in file
+#word_count = Hash.new(0)
+#file.each do |line|
+#	line.gsub(/[^a-zA-Z ]/,'').downcase.split(' ').each do |word|
+#		word_count[word] += 1
+#	end
+#end
+#word_count.sort_by{|k,v|-v}[0..10].each{|k,v| puts "#{k}: #{v}"}
 
-##Count occurences of single word in file
-word_count = Hash.new(0)
+#Bigram count
+bigram_count = Hash.new(0)
 file.each do |line|
-	line.gsub(/[^a-zA-Z ]/,'').downcase.split(' ').each do |word|
-		word_count[word] += 1 if word.length>0
+	line.gsub(/[^a-zA-Z ]/,'').downcase.split(' ').each_cons(2).to_a.each do |bigram|
+		bigram_count[bigram] += 1
 	end
 end
-word_count.sort_by{|k,v|-v}.each{|k,v| puts "#{k}: #{v}"}
+bigram_count.sort_by{|k,v|-v}[0..10].each{|k,v| puts "#{k}: #{v}"}
