3381560f78a9e1fb409da6b27f8a093802496b3c
Oscar Morrison <me@oscarmorrison.com>
2015-09-28 11:08:13 -0700

    Generate random sentence based on random bigram frequencies

--- 25LetterHistogram.rb
+++ 25LetterHistogram.rb
@@ -21,10 +21,58 @@ file = File.open("25in.txt","r").readlines
 #word_count.sort_by{|k,v|-v}[0..10].each{|k,v| puts "#{k}: #{v}"}
 
 #Bigram count
-bigram_count = Hash.new(0)
+$bigram_count = Hash.new(0)
 file.each do |line|
 	line.gsub(/[^a-zA-Z ]/,'').downcase.split(' ').each_cons(2).to_a.each do |bigram|
-		bigram_count[bigram] += 1
+		$bigram_count[bigram] += 1
 	end
 end
-bigram_count.sort_by{|k,v|-v}[0..10].each{|k,v| puts "#{k}: #{v}"}
+#bigram_count.sort_by{|k,v|-v}[0..10].each{|k,v| puts "#{k}: #{v}"}
+
+def return_bigram(word, bigram)
+	return bigram.select{|k,v| k[0].include? word}.sort_by{|k,v|-v}
+end
+
+#based on frequency
+def return_random_word(bigram)
+	words = []
+	bigram.each{|k,v| v.times{words<<k[1]}}
+#	puts words.size
+	return words.sample
+end
+
+def helper(word, bigram, i)
+	if i < 10
+		i +=1
+		word = return_random_word(return_bigram(word,$bigram_count))
+		print word + " "
+		return helper(word,return_bigram(word,$bigram_count),i)
+	else
+		return return_random_word(return_bigram(word,$bigram_count))
+	end
+end
+
+i = 0
+puts helper("said",$bigram_count,i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
